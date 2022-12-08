861cc3e6ae7cf0621c2098491d6245ecf9395b62
Benjamin DECONINCK <bdbenboss@hotmail.com>
2022-12-08 16:43:21 +0100

    association, validate et dependent

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  belongs_to :hotel
+  validates :capacity, presence: true
+  validates_numericality_of :capacity, :greater_than => 0
+  validates :price_per_night, presence: true
 end
